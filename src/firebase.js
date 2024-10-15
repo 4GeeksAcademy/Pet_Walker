ef39415520b4828346b50cae459b5cb22ac2f7ca
Pablo Dávila <167660504+PABLIUXDEV@users.noreply.github.com>
2024-10-15 02:07:39 +0000

    Funcion de upload para walker y owner

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import toast from "react-hot-toast";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,38 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+ const uploadImage = async (image) => {
+    const storage = getStorage();
+    const storageRef = ref(storage, 'images/${image.name}');
+
+    const metadata = {
+        contentType: image.type
+    };
+
+    try {
+        const fileData = await uploadBytesResumable(storageRef, image, metadata);
+        const downloadURL = await getDownloadURL(fileData.ref);
+        console.log("File available at", downloadURL);
+        return downloadURL
+    } catch (error) {
+        return null;
+    }
+ }
+
+ const registerUser = async (user) => {
+    if (user.Walker === 1) {
+        toast.error("Passwords do not match");
+    elseif (user.Owner === 2); {
+        toast.error("Passwords do not match");
+        return;
+    }
+
+    const profileImageURL = await uploadImage(user.image);
+    await actions.register(user.email, user.fullName, user.password, profileImageURL);
+    navigate("/");
+ }
+
+ }
+
+ export default uploadImage;   
