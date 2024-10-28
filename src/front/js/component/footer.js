9e8067353c43016e7cb57343da2e56ec992c49ff
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-10-28 23:47:38 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer py-3 text-center" style={{ backgroundColor: "#3F4551" }}>
-		<p  className="primaryText" style={{color: "#ffffff", fontSize: "25px"}}>
-			PET WALKER
-		</p>
+	<footer class="bg-dark text-white mt-5 " >
+		<div class="container py-4">
+			<div class="row">
+				<div class="col-md-4">
+					<h5>SOBRE NOSOTROS</h5>
+					<p>Somos una empresa dedicada a ofrecer los mejores servicios de calidad en el sector. Nuestro compromiso es con la satisfacción del cliente y la innovación constante.</p>
+				</div>
+				<div class="col-md-4">
+					<h5>Contacto</h5>
+					<p><strong>Teléfono:</strong> +1 234 567 890</p>
+					<p>
+						<a href="https://www.instagram.com/tu_instagram" target="_blank" class="text-white">
+							<i class="fab fa-instagram"></i> Instagram
+						</a>
+					</p>
+				</div>
+				<div class="col-md-4">
+					<h5>Redes Sociales</h5>
+					<ul class="list-unstyled">
+						<li>
+							<a href="https://www.instagram.com/tu_instagram" target="_blank" class="text-white">Instagram</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="text-center mt-4">
+				<small>&copy; 2024 PetWalker. Todos los derechos reservados.</small>
+			</div>
+		</div>
 	</footer>
 );
