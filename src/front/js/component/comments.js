8d123da3d37ac769a5556e14e03fbfa40df2f58d
Pablo Dávila <167660504+PABLIUXDEV@users.noreply.github.com>
2024-10-29 01:00:15 +0000

    ajustes al home

--- src/front/js/component/comments.js
+++ src/front/js/component/comments.js
@@ -4,23 +4,24 @@ import { Link } from "react-router-dom";
 export const Comments = () => {
     return (
 
-        <div className="container-fluid  d-flex justify-content-center align-self-start ">
+        <div className="container-fluid  d-flex justify-content-center">
 
-           <div className="card mx-2" style={{ border: "2px solid #E7F8F3", borderRadius: "0.25rem" }}>
+           <div className="card mx-2 mb-4" style={{ border: "2px solid #E7F8F3", borderRadius: "0.25rem" }}>
                 <div className="card-body">
                     <h4 className="card-title">Francisco</h4>
                     <h5 className="card-subtitle mb-2 text-muted">Paseador</h5>
                     <p className="card-text">"En ocasiones me encuentro saturado y entonces puedo remitir a otro compañero cuando me ofrecen un cliente."</p>
                 </div>
             </div>
-            <div className="card mx-2" style={{ border: "2px solid #E7F8F3", borderRadius: "0.25rem" }}>
+            
+            <div className="card mx-2 mb-4" style={{ border: "2px solid #E7F8F3", borderRadius: "0.25rem" }}>
                 <div className="card-body">
                     <h4 className="card-title">Alejandra</h4>
                     <h5 className="card-subtitle mb-2 text-muted">Dueña</h5>
                     <p className="card-text">"Es muy fácil encontrar paseadores y estar en contacto con ellos. Además, las experiencias con otros usuarios hace fácil elegir."</p>
                 </div>
             </div>
-            <div className="card mx-2" style={{ border: "2px solid #E7F8F3", borderRadius: "0.25rem" }}>
+            <div className="card mx-2 mb-4" style={{ border: "2px solid #E7F8F3", borderRadius: "0.25rem" }}>
                 <div className="card-body">
                     <h4 className="card-title">Fernando</h4>
                     <h5 className="card-subtitle mb-2 text-muted">Dueño</h5>
